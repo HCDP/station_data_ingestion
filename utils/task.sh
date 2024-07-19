f21adf06e5ffa25d4095237c5ee1354025131467
jardmclan <mcleanj@hawaii.edu>
2024-07-19 09:21:32 -1000

    fix token gen ep url ref

--- utils/task.sh
+++ utils/task.sh
@@ -2,16 +2,13 @@
 echo "[task.sh] Starting Execution."
 cd /home/hcdp_tapis_ingestor/station_values
 
-echo "[task.sh] [1/6] Downloading ingestion config."
+echo "[task.sh] [1/3] Downloading ingestion config."
 wget $INGESTION_CONFIG_URL -O config_temp.json
 
-echo "[task.sh] [2/6] Updating date strings in config if requested."
+echo "[task.sh] [2/3] Updating date strings in config if requested."
 python3 /actor/update_date_string_in_config.py config_temp.json config.json $CUSTOM_DATE
 
-echo "[task.sh] [5/6] Injecting authentication into config."
-python3 /actor/inject_auth.py config.json
-
-echo "[task.sh] [6/6] Ingesting station values."
+echo "[task.sh] [3/3] Ingesting station values."
 python3 driver.py config.json
 
 echo "[task.sh] All done!"
