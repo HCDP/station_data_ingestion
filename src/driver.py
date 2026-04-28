e057ce0cdc5978f329278f982e68874787aa60b8
jardmclan <mcleanj@hawaii.edu>
2026-04-28 10:03:13 -1000

    remove state tracing, cleanup, update example config, update config naming for v3

--- src/driver.py
+++ src/driver.py
@@ -1,10 +1,6 @@
 import csv
 import json
 from sys import stderr, argv, exit
-import sys
-import signal
-import sys
-from traceback import print_exception
 import requests
 from os.path import isfile
 from time import perf_counter
@@ -12,29 +8,6 @@ import asyncio
 
 from modules.ingestion_handler import V3Handler
 from modules.date_parser import DateParser, isoToDate
-
-
-def write_state(state_data, state_file):
-    if state_file is not None:
-        with open(state_file, "w") as f:
-            json.dump(state_data, f)
-
-def get_state(state_file):
-    #init state
-    #note row and column indices are to data rows/columns (does not include header row or metadata columns, cols should be offset from data col start and row should start at row 1)
-    state_data = {
-        "file": 0,
-        "complete": False
-    }
-    #if state file not provided return default, won't be written
-    if state_file is not None:
-        #if file does not exist return default init state
-        if isfile(state_file):
-            #otherwise set to saved state
-            with open(state_file) as f:
-                state_data = json.load(f)
-
-    return state_data
 
 
 ######
@@ -46,52 +19,20 @@ async def main():
             Invalid command line args. Must contain a config file.
 
             usage:
-            driver.py <config_file> [<state_file>]
+            driver.py <config_file>
         """, file = stderr)
         exit(1)
 
     config_file = argv[1]
-    state_file = None
-
-    if len(argv) > 2:
-        state_file = argv[2]
 
     config = None
     with open(config_file) as f:
         config = json.load(f)
-    state_data = get_state(state_file)
 
-
-    ##################################
-    ##################################
-
-    def uncaught_exception_handler(exctype, value, tb):
-        write_state(state_data, state_file)
-        print_exception(exctype, value, tb)
-        exit(1)
-
-    def unraisable_exception_handler(unraisable):
-        write_state(state_data, state_file)
-        print_exception(unraisable.exc_type, unraisable.exc_value, unraisable.exc_traceback)
-        exit(1)
-
-    sys.excepthook = uncaught_exception_handler
-    sys.unraisablehook = unraisable_exception_handler
-
-    def sig_handler(sig, frame):
-        print("Process interrupted or terminated")
-        write_state(state_data, state_file)
-        exit(2)
-
-    signal.signal(signal.SIGINT, sig_handler)
-    signal.signal(signal.SIGTERM, sig_handler)
-
-    ##################################
-    ##################################
 
     #deconstruct config
     data = config["data"]
-    tapis_config = config["tapis_config"]
+    tapis_config = config.get("tapisV3_config")
 
     tapis_handler = V3Handler(tapis_config)
 
@@ -140,149 +81,107 @@ async def main():
             raise Exception("Invalid date range")
 
         for i in range(len(files)):
-            
-            ################################
-            ########### profiler ###########
-            ################################
             start_time = perf_counter()
+
+            docs = []
+            file = files[i]
+            print(f"Processing file {file}")
+            #check if file exists on local system
+            is_local_file = isfile(file)
             
-            ################################
-            ################################
-            ################################
-            
-            #if file listed in state data is after this one skip
-            if i >= state_data["file"]:
-                docs = []
-                file = files[i]
-                print(f"Processing file {file}")
-                #check if file exists on local system
-                is_local_file = isfile(file)
+            fd = None
+            res = None
+            try:
+                #if local file open and assign fd to file ref
+                if is_local_file:
+                    print("Found local file.")
+                    fd = open(file, "r")
+                #otherwise try to get remote file
+                #if file does not exist or an error occurs the exception will be caught by the default exception handler and the program will terminate at the invalid file
+                else:
+                    print("Could not find local file. Attempting to open remote file.")
+                    res = requests.get(file, stream = True)
+                    res.raise_for_status()
+                    #csv requires text not bytes, decode (assumes utf-8)
+                    fd = (line.decode("utf-8") for line in res.iter_lines())
+                reader = csv.reader(fd)
+                dates = None
+                range_start = data_col_start
+                range_end = range_start
+                #current data row
+                row_num = 0
                 
-                fd = None
-                res = None
-                try:
-                    #if local file open and assign fd to file ref
-                    if is_local_file:
-                        print("Found local file.")
-                        fd = open(file, "r")
-                    #otherwise try to get remote file
-                    #if file does not exist or an error occurs the exception will be caught by the default exception handler and the program will terminate at the invalid file
-                    else:
-                        print("Could not find local file. Attempting to open remote file.")
-                        res = requests.get(file, stream = True)
-                        res.raise_for_status()
-                        #csv requires text not bytes, decode (assumes utf-8)
-                        fd = (line.decode("utf-8") for line in res.iter_lines())
-                    reader = csv.reader(fd)
-                    dates = None
-                    range_start = data_col_start
-                    range_end = range_start
-                    #current data row
-                    row_num = 0
-                    
-                    # process header
-                    header = next(reader)  
-                    dates = []
-                    #transform dates
-                    for i in range(len(header)):
-                        #if before start index, skip
-                        if i >= range_start:
-                            #create date parser from current date and period
-                            date_handler = DateParser(header[i], period)
-                            date = date_handler.getDatetime()
-                            date_s = date_handler.getISOString()
-                            if (start_date is None or date >= start_date) and (end_date is None or date <= end_date):
-                                dates.append(date_s)
-                                if date == start_date:
-                                    range_start = i
-                                range_end = i + 1
-                                    
-                    # process data rows
-                    for row in reader:
-                        # guard against empty rows or stubs
-                        if not row or len(row) <= range_start:
-                            continue
-                        station_id = row[id_col]
-                        #cut values to data range
-                        values = row[range_start:range_end]
-                        for col in range(len(values)):
-                            value = values[col]
-                            #if value is nodata skip
-                            if value != nodata:
-                                #transform to numeric
-                                value_f = float(value)
+                # process header
+                header = next(reader)  
+                dates = []
+                #transform dates
+                for i in range(len(header)):
+                    #if before start index, skip
+                    if i >= range_start:
+                        #create date parser from current date and period
+                        date_handler = DateParser(header[i], period)
+                        date = date_handler.getDatetime()
+                        date_s = date_handler.getISOString()
+                        if (start_date is None or date >= start_date) and (end_date is None or date <= end_date):
+                            dates.append(date_s)
+                            if date == start_date:
+                                range_start = i
+                            range_end = i + 1
                                 
-                                date = dates[col]
+                # process data rows
+                for row in reader:
+                    # guard against empty rows or stubs
+                    if not row or len(row) <= range_start:
+                        continue
+                    station_id = row[id_col]
+                    #cut values to data range
+                    values = row[range_start:range_end]
+                    for col in range(len(values)):
+                        value = values[col]
+                        #if value is nodata skip
+                        if value != nodata:
+                            #transform to numeric
+                            value_f = float(value)
+                            
+                            date = dates[col]
 
-                                data = {
-                                    "datatype": datatype,
-                                    "fill": fill,
-                                    "period": period,
-                                    "station_id": station_id,
-                                    "date": date,
-                                    "value": value_f
-                                }
+                            data = {
+                                "datatype": datatype,
+                                "fill": fill,
+                                "period": period,
+                                "station_id": station_id,
+                                "date": date,
+                                "value": value_f
+                            }
 
-                                #set up non-required props
-                                for prop_key, prop_value in additional_props.items():
-                                    data[prop_key] = prop_value
+                            #set up non-required props
+                            for prop_key, prop_value in additional_props.items():
+                                data[prop_key] = prop_value
 
-                                doc = {
-                                    "name": doc_name,
-                                    "value": data
-                                }
-                                docs.append(doc)
-                        row_num +=1
-                finally:
-                    if is_local_file and fd is not None:
-                        fd.close()
-                    elif not is_local_file and res is not None:
-                        res.close()
-                        
-                ################################
-                ########### profiler ###########
-                ################################
-                
-                end_time = perf_counter()
-                print(f"Completed parsing file {file}: Elapsed time: {end_time - start_time:.6f} seconds")
-                
-                ################################
-                ################################
-                ################################
-                
-                
-                ################################
-                ########### profiler ###########
-                ################################
-                
-                start_time = perf_counter()
-                print(f"Creating Tapis documents")
-                
-                ################################
-                ################################
-                ################################
-                
-                
-                stats = await tapis_handler.create_docs(docs, key_fields, replace_duplicates)
-                
-                ################################
-                ########### profiler ###########
-                ################################
-                
-                end_time = perf_counter()
-                print(f"Completed creating Tapis documents: Elapsed time: {end_time - start_time:.6f} seconds")
-                
-                ################################
-                ################################
-                ################################
-                
-                print(f"Completed processing {file}. Created: {stats['created']}, Replaced: {stats['replaced']}")
-                
-                state_data["file"] = i + 1
-                write_state(state_data, state_file)
-
-    state_data["complete"] = True
-    write_state(state_data, state_file)
+                            doc = {
+                                "name": doc_name,
+                                "value": data
+                            }
+                            docs.append(doc)
+                    row_num +=1
+            finally:
+                if is_local_file and fd is not None:
+                    fd.close()
+                elif not is_local_file and res is not None:
+                    res.close()
+            
+            end_time = perf_counter()
+            print(f"Completed parsing file {file}: Elapsed time: {end_time - start_time:.6f} seconds")
+            
+            start_time = perf_counter()
+            print(f"Creating Tapis documents")
+            
+            stats = await tapis_handler.create_docs(docs, key_fields, replace_duplicates)
+            
+            end_time = perf_counter()
+            print(f"Completed creating Tapis documents: Elapsed time: {end_time - start_time:.6f} seconds")
+            print(f"Completed processing {file}. Created: {stats['created']}, Replaced: {stats['replaced']}")
+            
     print("Complete!")
 
 
--- src/modules/ingestion_handler.py
+++ src/modules/ingestion_handler.py
@@ -1,7 +1,4 @@
-import requests
 import json
-from time import sleep
-import requests
 import json
 import random
 import urllib3
@@ -15,252 +12,6 @@ from tapipy.tapis import Tapis
 
 class RecordKeyException(Exception):
     pass
-
-
-class V2Handler:
-    def __init__(self, config):
-        self.__retry = config["retry"]
-        self.__agave_url = f"{config['tenant_url']}/meta/v2/data"
-        self.__hcdp_api_url = config["hcdp_api_url"]
-        self.__agave_headers = {
-            "Authorization": f"Bearer {config['agave_token']}",
-            "Content-Type": "application/json"
-        }
-
-        self.__hcdp_headers = {
-            "Authorization": f"Bearer {config['hcdp_api_token']}",
-            "Content-Type": "application/json"
-        }
-
-    def __req_with_retry(self, method, url, params, retry, delay = 0):
-        #pause for specified amount of time
-        sleep(delay)
-        
-        def retry_set_err(e):
-            #get backoff
-            backoff = self.__get_backoff(delay)
-            #decrease retry number
-            next_retry = retry - 1
-            #if have retries remaining try again return recursive result, otherwise just return error response
-            if next_retry >= 0:
-                return self.__req_with_retry(method, url, params, next_retry, backoff)
-            else:
-                return {
-                    "res": None,
-                    "error": e
-                }
-        res = None
-        try:
-            #may raise ConnectionError, res will be None if last failure is a connection error
-            res = method(url, **params)
-        #all request errors inherited from requests.exceptions.RequestException
-        except requests.exceptions.RequestException as e:
-            #retry request and set error
-            return retry_set_err(e)
-        try:
-            #will raise an HTTPError if request returned an error response
-            res.raise_for_status()
-        except requests.exceptions.HTTPError as e:
-            #retry request and set error
-            return retry_set_err(e)
-            
-        #return response
-        return {
-            "response": res,
-            "error": None
-        }
-            
-
-    def __get_backoff(self, delay):
-        backoff = 0
-        #if first failure backoff of 0.25-0.5 seconds
-        if delay == 0:
-            backoff = 0.25 + random.uniform(0, 0.25)
-        #otherwise 2-3x current backoff
-        else:
-            backoff = delay * 2 + random.uniform(0, delay)
-        return backoff
-
-
-    def __get_success(self, res):
-        status = res.status_code
-        status_group = status // 100
-        return status_group == 2
-
-
-    def retrieve_by_uuid(self, uuid):
-        url = f"{self.__agave_url}/{uuid}"
-        params = {
-            "headers": self.__agave_headers,
-            "verify": False
-        }
-        res_data = self.__req_with_retry(requests.get, url, params, self.__retry)
-        #if errored out raise last error
-        if res_data["error"] is not None:
-            raise res_data["error"]
-        
-        res = res_data["response"]
-        data = res.json()["result"]
-        return data
-
-
-    def query_data(self, data, limit = None, offset = None):
-        query = json.dumps(data)
-
-        params = {
-            "q": query
-        }
-
-        if limit is not None:
-            params["limit"] = limit
-        if offset is not None:
-            params["offset"] = offset
-
-        request_params = {
-            "params": params,
-            "headers": self.__agave_headers,
-            "verify": False
-        }
-
-        res_data = self.__req_with_retry(requests.get, self.__agave_url, request_params, self.__retry)
-
-        #if errored out raise last error
-        if res_data["error"] is not None:
-            raise res_data["error"]
-        
-        res = res_data["response"]
-        data = res.json()["result"]
-        return data
-
-
-    def query_uuids(self, data, limit = None, offset = None):
-        uuids = []
-        #get result of query
-        data = self.query_data(data, limit = limit, offset = offset)
-        #list uuids from matching records
-        for record in data:
-            uuids.append(record["uuid"])
-        return uuids
-
-
-    def check_duplicate(self, data, key_fields):
-        duplicate_data = {
-            "is_duplicate": False,
-            "changed": False,
-            "duplicate_uuid": None
-        }
-        key_data = {
-            "name": data["name"],
-        }
-
-        for field in key_fields:
-            key = f"value.{field}"
-            key_data[key] = data["value"][field]
-        matches = self.query_data(key_data)
-        #just throw an error if multiple, not used in any other way for now
-        if len(matches) > 1:
-            raise RecordKeyException("Multiple entries match the specified key data")
-        #python can compare dicts with ==
-        elif len(matches) == 1:
-            duplicate_data["is_duplicate"] = True
-            duplicate_data["duplicate_uuid"] = matches[0]["uuid"]
-            duplicate_data["changed"] = (matches[0]["value"] != data["value"])
-        return duplicate_data
-
-
-    def create_check_duplicates(self, data, key_fields, replace = True):
-        duplicate_data = self.check_duplicate(data, key_fields)
-
-        if not duplicate_data["is_duplicate"]:
-            self.create(data)
-        elif replace and duplicate_data["changed"]:
-            self.replace(data, duplicate_data["duplicate_uuid"])
-            
-
-    def delete(self, uuid):
-        delete_endpoint = f"{self.__hcdp_api_url}/db/delete"
-        payload = {
-            "uuid": uuid
-        }
-        payload = json.dumps(payload)
-
-        request_params = {
-            "data": payload,
-            "headers": self.__hcdp_headers,
-            "verify": False
-        }
-
-        #wrap request in retry and get response
-        res_data = self.__req_with_retry(requests.post, delete_endpoint, request_params, self.__retry)
-
-        #if errored out raise last error
-        if res_data["error"] is not None:
-            raise res_data["error"]
-    
-
-    def bulkDelete(self, uuids):
-        delete_endpoint = f"{self.__hcdp_api_url}/db/bulkDelete"
-        payload = {
-            "uuids": uuids
-        }
-        payload = json.dumps(payload)
-
-        request_params = {
-            "data": payload,
-            "headers": self.__hcdp_headers,
-            "verify": False
-        }
-
-        #wrap request in retry and get response
-        res_data = self.__req_with_retry(requests.post, delete_endpoint, request_params, self.__retry)
-
-        #if errored out raise last error
-        if res_data["error"] is not None:
-            raise res_data["error"]
-
-    
-    def create(self, data):
-        payload = json.dumps(data)
-
-        request_params = {
-            "data": payload,
-            "headers": self.__agave_headers,
-            "verify": False
-        }
-
-        #wrap request in retry and get response
-        res_data = self.__req_with_retry(requests.post, self.__agave_url, request_params, self.__retry)
-
-        #if errored out raise last error
-        if res_data["error"] is not None:
-            raise res_data["error"]
-
-
-    def replace(self, data, uuid):
-        replace_endpoint = f"{self.__hcdp_api_url}/db/replace"
-        payload = {
-            "uuid": uuid,
-            "value": data["value"]
-        }
-        payload = json.dumps(payload)
-
-        request_params = {
-            "data": payload,
-            "headers": self.__hcdp_headers,
-            "verify": False
-        }
-
-        #wrap request in retry and get response
-        res_data = self.__req_with_retry(requests.post, replace_endpoint, request_params, self.__retry)
-
-        #if errored out raise last error
-        if res_data["error"] is not None:
-            raise res_data["error"]
-        
-        
-        
-        
-
 
 
 class V3Handler:
@@ -452,18 +203,8 @@ class V3Handler:
     
 
     async def create_docs(self, data, key_fields, replace = True, db = None, collection = None):
-        
-        ################################
-        ########### profiler ###########
-        ################################
-        
         start_time = perf_counter()
         print(f"Querying duplicate documents")
-        
-        ################################
-        ################################
-        ################################
-        
         
         if db is None:
             db = self.__db
@@ -482,75 +223,29 @@ class V3Handler:
                 replace_docs[uuid] = doc
             elif action == "create":
                 create_docs.append(doc)
-                
-
-        ################################
-        ########### profiler ###########
-        ################################
         
         end_time = perf_counter()
         print(f"Completed querying duplicate documents: Elapsed time: {end_time - start_time:.6f} seconds")
         
-        ################################
-        ################################
-        ################################
-                
-        
-        
-        ################################
-        ########### profiler ###########
-        ################################
-        
         start_time = perf_counter()
         print(f"Replacing {len(replace_docs)} documents")
         
-        ################################
-        ################################
-        ################################
-        
         tasks = [self.__replace(uuid, replace_docs[uuid], db, collection) for uuid in replace_docs]
         await asyncio.gather(*tasks)
-            
-        
-        ################################
-        ########### profiler ###########
-        ################################
         
         end_time = perf_counter()
         print(f"Completed replacing duplicate documents: Elapsed time: {end_time - start_time:.6f} seconds")
         
-        ################################
-        ################################
-        ################################
-        
-        
-        ################################
-        ########### profiler ###########
-        ################################
-        
         start_time = perf_counter()
         print(f"Creating {len(create_docs)} documents")
-        
-        ################################
-        ################################
-        ################################
             
         if len(create_docs) > 1:
             await self.__create(create_docs, db, collection)
         elif len(create_docs) > 0:
             await self.__create(create_docs[0], db, collection)
-            
-
-        ################################
-        ########### profiler ###########
-        ################################
         
         end_time = perf_counter()
         print(f"Completed creating documents: Elapsed time: {end_time - start_time:.6f} seconds")
-        
-        ################################
-        ################################
-        ################################
         
             
         return {
